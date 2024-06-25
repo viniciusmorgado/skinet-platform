c7fbbc29b4b4cd4bd22cd7c4b90c4f795011dd18
Vinicius Morgado <34577818+viniciusmorgado@users.noreply.github.com>
2024-06-25 07:58:37 -0300

    Add a navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   // TODO: New components need to be declared inside the declarations array
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  declarations: [AppComponent, NavBarComponent],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule, 
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatMenuModule, 
+    FlexLayoutModule, 
+    MatIconModule
+  ],
+  providers: [provideAnimationsAsync()],
   // TODO: Define which component will bootstrap the application tree (of components)
   bootstrap: [AppComponent],
 })
